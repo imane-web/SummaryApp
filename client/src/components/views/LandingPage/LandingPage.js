bf2298476884ff85c7bc8c05a5ce245090420875
eimane <imaneelmahdi99@gmail.com>
2021-02-16 14:20:18 +0100

    style editing

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -31,7 +31,7 @@ function LandingPage() {
         var seconds = Math.floor(video.duration - minutes * 60);
 
         return <Col lg={6} md={8} xs={24}>
-            <div style={{ position: 'relative' }}>
+            <div className="imane" style={{ position: 'relative' }}>
                 <a href={`/video/${video._id}`} >
                 <img style={{ width: '100%' }} alt="thumbnail" src={`http://localhost:5000/${video.thumbnail}`} />
                 <div className=" duration"
@@ -59,7 +59,7 @@ function LandingPage() {
 
 
     return (
-        <div className="imane" style={{ width: '85%', margin: '3rem auto' }}>
+        <div style={{ width: '85%', margin: '3rem auto' }}>
             <Title level={2} > Recommended </Title>
             <hr />
 
